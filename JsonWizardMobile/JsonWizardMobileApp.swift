e9235a1e0481e77338242f1ada48ef6054aba9ae
Igor Pajkert <pajkert.igor@gmail.com>
2024-12-24 03:57:42 +0100

    Initial Commit

---
+++ JsonWizardMobile/JsonWizardMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  JsonWizardMobileApp.swift
+//  JsonWizardMobile
+//
+//  Created by Igor Pajkert on 24/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct JsonWizardMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
