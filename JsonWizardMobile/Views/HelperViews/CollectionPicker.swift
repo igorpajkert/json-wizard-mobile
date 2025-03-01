35e8d3b0fe3337929acf29b13083f38e81d1c7ca
Igor <pajkert.igor@gmail.com>
2025-03-02 00:17:15 +0100

    Update | Admin Collection Picker

--- JsonWizardMobile/Views/HelperViews/CollectionPicker.swift
+++ JsonWizardMobile/Views/HelperViews/CollectionPicker.swift
@@ -9,16 +9,30 @@ import SwiftUI
 
 struct CollectionPicker: View {
     
+    @Environment(\.store) private var store
+    
     @Binding var selection: DataStore.CollectionType
     
     var body: some View {
-        Picker("picker_collection", selection: $selection) {
-            ForEach(DataStore.CollectionType.allCases) { type in
-                Text(type.rawValue)
-                    .tag(type as DataStore.CollectionType)
+        VStack {
+            Picker("picker_collection", selection: $selection) {
+                ForEach(DataStore.CollectionType.allCases) { type in
+                    Text(type.rawValue)
+                        .tag(type as DataStore.CollectionType)
+                }
+            }
+            .pickerStyle(.segmented)
+            HStack {
+                Label("label_categories_count", systemImage: "square.stack.3d.up")
+                Spacer()
+                Text(String(store.categories.count))
+            }
+            HStack {
+                Label("label_questions_count", systemImage: "rectangle.stack")
+                Spacer()
+                Text(String(store.questions.count))
             }
         }
-        .pickerStyle(.segmented)
     }
 }
 
