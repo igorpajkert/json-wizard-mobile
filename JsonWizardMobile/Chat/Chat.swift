d3a22a9a958a4033011670fd88b18a24340a09c5
Igor <pajkert.igor@gmail.com>
2025-03-15 20:25:40 +0100

    Feature | Chat backend

---
+++ JsonWizardMobile/Chat/Chat.swift
@@ -0,0 +1,72 @@
+//
+//  Chat.swift
+//  JsonWizardMobile
+//
+//  Created by Igor Pajkert on 15/03/2025.
+//
+
+import Foundation
+import FirebaseFirestore
+
+@Observable
+class Chat {
+    
+    private(set) var messages = [Message]()
+    private(set) var lastMessageId = 0
+    
+    private var listener: ListenerRegistration?
+    private let db = Firestore.firestore()
+    private let collection = "messages"
+    
+    init() {
+        listener = attachMessagesListener()
+    }
+    
+    deinit {
+        listener?.remove()
+    }
+    
+    private func attachMessagesListener() -> ListenerRegistration {
+        return db.collection(collection).addSnapshotListener { querySnapshot, error in
+            
+            guard let documents = querySnapshot?.documents else {
+                print("Error fetching documents: \(String(describing: error))")
+                return
+            }
+            
+            self.messages = documents.compactMap { document -> Message? in
+                do {
+                    return try document.data(as: Message.self)
+                } catch {
+                    print("Error decoding document: \(error)")
+                    return nil
+                }
+            }
+            
+            self.messages.sort { $0.timestamp < $1.timestamp }
+            
+            if let id = self.messages.last?.id {
+                self.lastMessageId = id
+            }
+        }
+    }
+    
+    func sendMessage(with text: String) {
+        do {
+            guard let userId = Authentication.shared.user?.uid else { return }
+            guard let userName = Authentication.shared.userData?.name else { return }
+            
+            let message = Message(
+                text: text,
+                senderId: userId,
+                senderName: userName,
+                timestamp: .now
+            )
+            
+            try db.collection(collection).document().setData(from: message)
+            
+        } catch {
+            print("Error sending message: \(error)")
+        }
+    }
+}
---
+++ JsonWizardMobile/Models/Chat/Message.swift
@@ -0,0 +1,31 @@
+//
+//  Message.swift
+//  JsonWizardMobile
+//
+//  Created by Igor Pajkert on 15/03/2025.
+//
+
+import Foundation
+import FirebaseFirestore
+
+struct Message: Identifiable, Codable {
+    let id: Int
+    var text: String
+    let senderId: String
+    let senderName: String
+    var timestamp: Date
+    
+    init(
+        id: Int = Int.randomID(),
+        text: String,
+        senderId: String,
+        senderName: String,
+        timestamp: Date
+    ) {
+        self.id = id
+        self.text = text
+        self.senderId = senderId
+        self.senderName = senderName
+        self.timestamp = timestamp
+    }
+}
